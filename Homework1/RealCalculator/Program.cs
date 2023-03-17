cad0279309b268b41ed8e20995aba48a78995db7
MIvanna <119980550+MIvanna@users.noreply.github.com>
2023-03-17 11:04:47 +0100

    Add files via upload

---
+++ Homework1/RealCalculator/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace RealCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+
+            Console.WriteLine("Real Calculator");
+
+            Console.Write("Please enter the first number: ");
+            string firstNum = Console.ReadLine();
+
+            Console.Write("Please enter the second number: ");
+            string secondNum = Console.ReadLine();
+
+            bool firstNumParse = double.TryParse(firstNum, out double firstNumber);
+            bool secondNumParse = double.TryParse(secondNum, out double secondNumber);
+
+            if (!firstNumParse || !secondNumParse)
+            {
+                Console.WriteLine("Wrong input. Please enter a number!");
+                return;
+            }
+
+            Console.Write("Please enter what operation to preform ( +, - , * , / ): ");
+            string operation = Console.ReadLine();
+
+            double result;
+
+            switch (operation)
+            {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    Console.WriteLine($"The result is: {result}");
+                    break;
+
+                case "-":
+                    result = firstNumber - secondNumber;
+                    Console.WriteLine($"The result is: {result}");
+                    break;
+
+                case "*":
+                    result = firstNumber * secondNumber;
+                    Console.WriteLine($"The result is: {result}");
+                    break;
+
+                case "/":
+
+                    result = firstNumber / secondNumber;
+
+                    if (secondNumber == 0)
+                    {
+                        Console.WriteLine("Can not divide by 0");
+                        return;
+                    }
+                    Console.WriteLine($"The result is: {result}");
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter valid input!");
+                    break;
+            }
+        }
+    }
+}
---
+++ Homework1/SwapNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SwapNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 3
+
+            Console.WriteLine("Swap Numbers");
+
+            Console.Write("Input the First Number: ");
+            string firstNum = Console.ReadLine();
+
+            Console.Write("Input the Second Number: ");
+            string secondNum = Console.ReadLine();
+
+            bool firstNumParse = int.TryParse(firstNum, out int firstNumber);
+            bool secondNumParse = int.TryParse(secondNum, out int secondNumber);
+
+            if (!firstNumParse || !secondNumParse)
+            {
+                Console.WriteLine("Error occured. Invalid input.");
+            }
+            else
+            {
+                Console.WriteLine("After Swapping: ");
+                Console.WriteLine($"First Number: {secondNumber}");
+                Console.WriteLine($"Second Number: {firstNumber}");
+            }
+        }
+    }
+}
