f357d4b433869adbeb6ab331a53b7d67025fae42
MIvanna <119980550+MIvanna@users.noreply.github.com>
2023-03-17 21:17:04 +0100

    Add files via upload

---
+++ Homework2/StudentGroup/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 2
+
+            string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+
+            string[] studentsG2 = { "Zdravka", "Petkana", "Stanka", "Branka", "Trajana" };
+
+            Console.WriteLine("Enter student group: (there are 1 and 2)");
+
+            if (!int.TryParse(Console.ReadLine(), out int decision))
+            {
+                Console.WriteLine("Error occured! Please enter 1 or 2:");
+            }
+            else if (decision == 1)
+            {
+                Console.WriteLine("The Students in G1 are:");
+                foreach (string student in studentsG1)
+                {
+                    Console.WriteLine($"{student}");
+                }
+            }
+            else if (decision == 2)
+            {
+                Console.WriteLine("The Students in G2 are:");
+                foreach (string student in studentsG2)
+                {
+                    Console.WriteLine($"{student}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+        }
+    }
+}
---
+++ Homework2/SumOfEven/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum Of Even");
+
+            int[] numbers = new int[7];
+            int result = 0;
+
+            for (int i = 1; i < 7; i++)
+            {
+                Console.WriteLine($"Enter integer no. {i}:");
+                //  string input = Console.ReadLine();
+                //  bool numParsed = int.TryParse(input, out numbers[i]);
+
+                if (!int.TryParse(Console.ReadLine(), out numbers[i]))
+                {
+                    Console.WriteLine("Error occured!");
+                    return;
+                }
+            }
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    result += number;
+                }
+            }
+
+            Console.WriteLine($"The result is: {result}");
+        }
+    }
+}
