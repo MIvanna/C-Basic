194189771388b70efd595b640f9788bd8e43926a
MIvanna <119980550+MIvanna@users.noreply.github.com>
2023-03-17 10:32:17 +0100

    Add files via upload

---
+++ Homework 1/SwapNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SwapNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Task 3
+
+            Console.WriteLine("Swap Numbers");
+
+            Console.Write("Input the First Number: ");
+            string firstNum = Console.ReadLine();
+
+            Console.Write("Input the Second Number: ");
+            string secondNum = Console.ReadLine();
+
+            bool firstNumParse = int.TryParse(firstNum, out int firstNumber);
+            bool secondNumParse = int.TryParse(secondNum, out int secondNumber);
+
+            if (!firstNumParse || !secondNumParse)
+            {
+                Console.WriteLine("Error occured. Invalid input.");
+            }
+            else
+            {
+                Console.WriteLine("After Swapping: ");
+                Console.WriteLine($"First Number: {secondNumber}");
+                Console.WriteLine($"Second Number: {firstNumber}");
+            }
+        }
+    }
+}
