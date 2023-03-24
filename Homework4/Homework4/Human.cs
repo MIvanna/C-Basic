ec96512d6f1c1f16670697a0f51c1e1eb651c729
MIvanna <119980550+MIvanna@users.noreply.github.com>
2023-03-24 12:05:05 +0100

    Add files via upload

---
+++ Homework4/Homework4/Human.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Homework4
+{
+    internal class Human
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public string GetPersonStats()
+        {
+            return $"{FirstName} {LastName} {Age}";
+        }
+        
+    }
+}
---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human1 = new Human();
+            Console.WriteLine("Please enter your name:");
+            human1.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name:");
+            human1.LastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age:");
+            if (!int.TryParse(Console.ReadLine(), out int age))
+            {
+                Console.WriteLine("Invalid input. Please enter correct age.");
+            }
+            else
+            {
+                human1.Age = age;
+            }
+
+            Console.WriteLine(human1.GetPersonStats());
+
+        }
+    }
+}
